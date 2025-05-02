e2b2188f743fd899f7743e951bfc25ecc831b1e6
PostMail-hub <gmlals2949@naver.com>
2025-05-02 10:44:09 +0900

    feat : 플레이어 점프와 NPC 추가
    - 엘프 npc와 충돌 효과 구현
    - 플레이어의 스페이스바 버튼을 인식하여 바라보는 방향으로 빠르게 움직이는 기능 추가

--- Assets/Scripts/Entity/BaseController.cs
+++ Assets/Scripts/Entity/BaseController.cs
@@ -7,7 +7,6 @@ public class BaseController : MonoBehaviour
     protected Rigidbody2D _rigidbody;
     protected AnimationHandler animationHandler;
 
-
     [SerializeField] private SpriteRenderer characterRenderer;
 
     protected Vector2 movementDirection = Vector2.zero;
@@ -15,6 +14,18 @@ public class BaseController : MonoBehaviour
 
     protected Vector2 lookDirection = Vector2.zero;
     public Vector2 LookDirection { get { return lookDirection; } }
+
+    private bool isJumpingForward = false; 
+    private float jumpDuration = 0.1f; 
+    private float jumpTimer = 0f; // 시간 재기용
+
+    private bool isJumpOnCooldown = false; // 점프 쿨타임인지 아닌지
+    private float jumpCooldown = 1.0f; // 점프 쿨타임
+    private float jumpCooldownTimer = 0f; // 시간 재기용
+
+    private float jumpSpeed = 15f; // 점프의 속도
+
+
 
     protected virtual void Awake()
     {
@@ -32,16 +43,65 @@ public class BaseController : MonoBehaviour
     {
         HandleAction();
         Rotate(lookDirection);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            TryJump();
+        }
+
+        if (isJumpOnCooldown)
+        {
+            jumpCooldownTimer -= Time.deltaTime;
+            if (jumpCooldownTimer <= 0f)
+            {
+                isJumpOnCooldown = false;
+            }
+        }
     }
 
     protected virtual void FixedUpdate()
     {
-        Movment(movementDirection);
+        Vector2 velocity;
+
+        if (isJumpingForward)
+        {
+            jumpTimer -= Time.fixedDeltaTime;
+            if (jumpTimer <= 0f)
+            {
+                isJumpingForward = false;
+            }
+
+            velocity = lookDirection.normalized * jumpSpeed;
+        }
+        else
+        {
+            velocity = movementDirection * 5;
+        }
+
+        _rigidbody.velocity = velocity;
+        animationHandler.Move(new Vector2(velocity.x, 0));
     }
 
     protected virtual void HandleAction()
     {
 
+    }
+
+    private void TryJump() // 점프를 시도
+    {
+        if (!isJumpingForward && !isJumpOnCooldown) 
+        {
+            Jump(); // 점프 이후 값 설정
+        }
+    }
+
+    private void Jump() // 점프 이후 값 설정
+    {
+        isJumpingForward = true;
+        jumpTimer = jumpDuration;
+
+        isJumpOnCooldown = true; // 현재 점프를 쿨타임으로 설정
+        jumpCooldownTimer = jumpCooldown; // 쿨타임 타이머 시작
     }
 
     private void Movment(Vector2 direction)
